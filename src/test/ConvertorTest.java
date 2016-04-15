a04a5d18cfb3b9977de5e67f56648ee088ebbf0e
Dijana <deny.dijana@gmail.com>
2016-04-15 10:33:29 +0200

    Testing inch method is done

--- src/test/ConvertorTest.java
+++ src/test/ConvertorTest.java
@@ -15,12 +15,11 @@ public class ConvertorTest {
 	public void setUp() throws Exception {
 		convert = new Convertor();
 	}
+
 	@After
 	public void tearDown() throws Exception {
 		convert = null;
 	}
-		
-
 
 	@Test
 	public void testCent2Inch() {
@@ -29,8 +28,17 @@ public class ConvertorTest {
 		double result = (cent * formula);
 
 		assertEquals(1.57480316, result, 1e-15);
-		
-		
+
+	}
+	@Test
+	public void testInch2Cent() {
+		double inch = 2;
+		double formula = 2.54;
+		double result = (inch * formula);
+
+		assertEquals(5.08, result, 1e-15);
+
+
+	
 	}
 
-}
